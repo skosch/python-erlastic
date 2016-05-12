00114efc1ede202c856e767d015e4e0922ea9556
Johan Sommerfeld <johan@s2hc.com>
2016-05-12 12:20:51 +0200

    pep8 fixes on __init__.py

--- erlastic/__init__.py
+++ erlastic/__init__.py
@@ -1,4 +1,6 @@
 """Erlang External Term Format serializer/deserializer"""
+import struct
+import sys
 
 from erlastic.codec import ErlangTermDecoder, ErlangTermEncoder
 from erlastic.types import *
@@ -6,23 +8,24 @@ from erlastic.types import *
 encode = ErlangTermEncoder().encode
 decode = ErlangTermDecoder().decode
 
-import struct
-import sys
 
 def mailbox_gen():
-  while True:
-    len_bin = sys.stdin.buffer.read(4)
-    if len(len_bin) != 4: return
-    (length,) = struct.unpack('!I',len_bin)
-    yield decode(sys.stdin.buffer.read(length))
+    while True:
+        len_bin = sys.stdin.buffer.read(4)
+        if len(len_bin) != 4:
+            return
+        (length,) = struct.unpack('!I', len_bin)
+        yield decode(sys.stdin.buffer.read(length))
+
 
 def port_gen():
-  while True:
-    term = encode((yield))
-    sys.stdout.buffer.write(struct.pack('!I',len(term)))
-    sys.stdout.buffer.write(term)
+    while True:
+        term = encode((yield))
+        sys.stdout.buffer.write(struct.pack('!I', len(term)))
+        sys.stdout.buffer.write(term)
+
 
 def port_connection():
-  port = port_gen()
-  next(port)
-  return mailbox_gen(),port
+    port = port_gen()
+    next(port)
+    return mailbox_gen(), port
